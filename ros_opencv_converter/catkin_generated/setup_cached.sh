2a44b8960792351b82435e4a9e26439d3dc58de2
Jerome <jerome.dekerautem@hotmail.fr>
2016-04-06 20:27:37 +0200

    New package containing the face detection part of the project.

---
+++ ros_opencv_converter/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/lib:/home/jerome/catkin_ws/src/ros_opencv_converter/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/lib/pkgconfig:/home/jerome/catkin_ws/src/ros_opencv_converter/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/share/maven"
+export ROS_MAVEN_PATH="/home/jerome/catkin_ws/src/ros_opencv_converter/devel/share/maven:$ROS_MAVEN_PATH"
+export ROS_PACKAGE_PATH="/home/jerome/catkin_ws/src/ros_opencv_converter:$ROS_PACKAGE_PATH"
