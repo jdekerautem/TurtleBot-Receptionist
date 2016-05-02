69b267dc4f8062854521d8edbc06947d76d94b69
vbansal1 <vaibhavbansal.2791@gmail.com>
2016-05-02 08:41:53 -0700

    Added files via upload

--- notsotalkative.py
+++ notsotalkative.py
@@ -16,13 +16,7 @@ from sound_play.libsoundplay import SoundClient
 
 class voice_cmd_vel:
     def __init__(self):
-        rospy.on_shutdown(self.cleanup)
-        self.max_speed = rospy.get_param("~max_speed", 0.4)
-        self.max_angular_speed = rospy.get_param("~max_angular_speed", 1.5)
-        self.speed = rospy.get_param("~start_speed", 0.1)
-        self.angular_speed = rospy.get_param("~start_angular_speed", 0.5)
-        self.linear_increment = rospy.get_param("~linear_increment", 0.05)
-        self.angular_increment = rospy.get_param("~angular_increment", 0.4)
+        
         self.rate = rospy.get_param("~rate", 5)
         r = rospy.Rate(self.rate)
         self.paused = False
@@ -37,31 +31,39 @@ class voice_cmd_vel:
         self.soundhandle.stopAll()
         
         
-        
-        # Initialize the Twist message we will publish.
-        self.msg = Twist()
-
-        # Publish the Twist message to the cmd_vel topic
-        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        
         # Subscribe to the /recognizer/output topic to receive voice commands.
         rospy.Subscriber('/recognizer/output', String, self.speechCb)
         
         # A mapping from keywords to commands.
         self.keywords_to_command = {'stop': ['stop', 'halt', 'abort', 'kill', 'panic', 'off', 'freeze', 'shut down', 'turn off', 'help', 'help me'],
                                     'bye': ['bye', 'cheers', 'goodbye', 'see you', 'bye'],
-                                    'cafe' : ['cafe', 'campus'],
-                                    'hello': ['hi', 'hey', 'hello'],
+                                    'cafe' : ['cafe', 'campus', 'tea', 'coffee', 'eat'],
+				    'hello': ['hi', 'hey', 'hello'],
                                     'help' : ['help me', 'can help', 'help'],
                                     'name' : ['your name', 'name'],                             
-                                    'what' : ['what is', 'what is your', 'what'],
-                                    'wash' : ['washroom'],
-                                    'library' : ['library'],
+                                    'wash' : ['washroom', 'toilet'],
+                                    'library' : ['library', 'book', 'borrow'],
                                     'labs' : ['labs'],
                                     'talk': ['talk to me?', 'really talk?', 'you talk', 'you really talk?', 'talk'],                      
-                                    'how' : ['can you', 'can', 'how can', 'how can you'],
-                                    'cute' : ['cute', 'so cute'],
-                                    'amazing' : ['amazing', 'wonderful'],                                    
+                                    'amazing' : ['amazing', 'wonderful'], 
+				    'psychology' : ['psychology'],
+				    'teaching' : ['teaching', 'music'],
+				    'engineering' : ['engineering'],
+				    'biology' : ['biology', 'english', 'chemistry'],
+				    'maths' : ['computing', 'mathematics'],
+				    'geo' : ['geology', 'geography'],
+				    'marine' : ['marine'],
+				    'art' : ['art'],
+    				    'roland' : ['reception', 'architecture'],
+				    'business' : ['business'],
+				    'staff' : ['staff'],
+				    'sports' : ['sports'],
+  				    'robots' : ['robotics', 'robots'],
+				    'visit' : ['visit', 'to do'],
+				    'supermarket' : ['shop', 'supermarket'],
+			     	    'cashpoint' : ['cash points', 'ATM', 'cash machines'],
+				    'day' : ['day', 'today'],
+				    'weather' : ['weather'],                                 
                                     'pause': ['pause speech'],
                                     'continue': ['continue speech']}
         
@@ -69,7 +71,6 @@ class voice_cmd_vel:
         
         # We have to keep publishing the cmd_vel message if we want the robot to keep moving.
         while not rospy.is_shutdown():
-            self.cmd_vel_pub.publish(self.msg)
             r.sleep()                       
             
     	
@@ -93,20 +94,72 @@ class voice_cmd_vel:
         if self.paused:
             return       
         
-        if command == 'hello':    
-            self.soundhandle.say("Greetings!.", self.voice)
+        if command == 'hello':
+		self.soundhandle.say("Greetings!.", self.voice)
                 
-        if command == 'help':    
-            self.soundhandle.say("Ask me questons about the faculty. or your way around the faculty building.", self.voice)
+        if command == 'help':
+		self.soundhandle.say("Ask me questions", self.voice)
                     	
         if command == 'talk':
         	self.soundhandle.say("yes, I can", self.voice)
         	        	
- 		if command == 'bye':
- 			self.soundhandle.say("Bye Bye", self.voice)
+ 	if command == 'bye':
+ 		self.soundhandle.say("Bye Bye", self.voice)
         
-        if command == 'cute':
-        	self.soundhandle.say("Thank you", self.voice)
+        if command == 'weather':
+		self.soundhandle.say("I Don't know.", self.voice)
+        
+	if command == 'supermarket':
+		self.soundhandle.say("The nearest supermarket is the TESCO!. ", self.voice)
+	
+	if command == 'day':
+		self.soundhandle.say("It's tuesday!.", self.voice)
+
+	if command == 'psychology':
+		self.soundhandle.say("It's in link building!", self.voice)
+
+	if command == 'teaching':
+		self.soundhandle.say("the rolle building!.", self.voice)
+
+	if command == 'engineering':
+		self.soundhandle.say("That's right here!.", self.voice)
+
+	if command == 'biology':
+		self.soundhandle.say("It's is in the Davy building!.!", self.voice)
+	
+	if command == 'maths':
+		self.soundhandle.say("In the babbage building!.!", self.voice)
+
+	if command == 'geo':
+		self.soundhandle.say("It's in the Fitzroy building!.!", self.voice)
+
+	if command == 'marine':
+		self.soundhandle.say("In the reynolds And the marine building.! ", self.voice)
+	
+	if command == 'art':
+		self.soundhandle.say(" in the scott building!.!", self.voice)
+	
+	if command == 'roland':
+		self.soundhandle.say(" in the roland levinsky building!.!", self.voice)
+	
+	if command == 'business':
+		self.soundhandle.say("should be cookworthy building!", self.voice)
+
+	if command == 'staff':
+		self.soundhandle.say("In the Portland Square building!", self.voice)
+
+	if command == 'sports':
+		self.soundhandle.say("It's the Nancy Astor building. ", self.voice)
+
+	if command == 'robots':
+		self.soundhandle.say("in Smeaton's building or in Portland Square. !", self.voice)	
+
+	if command == 'cashpoint':
+		self.soundhandle.say("There are some on the eastern exit of this building.!!", self.voice)
+
+	if command == 'visit':
+		self.soundhandle.say("Well, you can walk along the seashore. May be.!", self.voice)
+
             
         if command == 'name':
         	self.soundhandle.say("charlie.", self.voice)
@@ -115,21 +168,24 @@ class voice_cmd_vel:
        		self.soundhandle.say("thank you so much.", self.voice)           
         
         if command == 'cafe':
-        	self.soundhandle.say("you can eat or have coffee at the SU shop.", self.voice)      
+        	self.soundhandle.say(" at the S U shop.", self.voice)      
        
         if command == 'wash':
-        	self.soundhandle.say("The Gents toilet can be found on the second floor. and a ladies toilet on the third floor.", self.voice)
+        	self.soundhandle.say("the second floor and the third floor.", self.voice)
     
         if command == 'library':
-        	self.soundhandle.say("As you will exit out of the Smeaton's building. Take a left. The building in front of you is the library.", self.voice)
+        	self.soundhandle.say("It's next to the Smeaton's building.", self.voice)
         
         if command == 'labs':
-        	self.soundhandle.say("The labs are on third floor of the Smeaton's building.", self.voice)
+        	self.soundhandle.say(" on the third floor.", self.voice)
+
+	
+
+	
     
     def cleanup(self):
         # When shutting down be sure to stop the robot!  Publish a Twist message consisting of all zeros.
-        twist = Twist()
-        self.cmd_vel_pub.publish(twist)
+        rospy.loginfo("Shutting Down..")
 		
 		
 if __name__=="__main__":
--- talkative.py
+++ talkative.py
@@ -16,13 +16,7 @@ from sound_play.libsoundplay import SoundClient
 
 class talkative:
     def __init__(self):
-        rospy.on_shutdown(self.cleanup)
-        self.max_speed = rospy.get_param("~max_speed", 0.4)
-        self.max_angular_speed = rospy.get_param("~max_angular_speed", 1.5)
-        self.speed = rospy.get_param("~start_speed", 0.1)
-        self.angular_speed = rospy.get_param("~start_angular_speed", 0.5)
-        self.linear_increment = rospy.get_param("~linear_increment", 0.05)
-        self.angular_increment = rospy.get_param("~angular_increment", 0.4)
+        
         self.rate = rospy.get_param("~rate", 5)
         r = rospy.Rate(self.rate)
         self.paused = False
@@ -36,34 +30,43 @@ class talkative:
         rospy.sleep(1)
         self.soundhandle.stopAll()
         
-          
-               
+        self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+        rospy.sleep(1)
         
-        
-        # Initialize the Twist message we will publish.
-        self.msg = Twist()
-
-        # Publish the Twist message to the cmd_vel topic
-        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        
+              
         # Subscribe to the /recognizer/output topic to receive voice commands.
         rospy.Subscriber('/recognizer/output', String, self.speechCb)
         
         # A mapping from keywords to commands.
-        self.keywords_to_command = {'stop': ['stop', 'halt', 'abort', 'kill', 'panic', 'off', 'freeze', 'shut down', 'turn off', 'help', 'help me'],
+        self.keywords_to_command = {'stop': ['stop', 'halt', 'abort', 'kill', 'panic', 'off', 'freeze', 'shut down', 'turn off'],
                                     'bye': ['bye', 'cheers', 'goodbye', 'see you', 'bye'],
-                                    'cafe' : ['cafe', 'campus'],
-                                    'hello': ['hi', 'hey', 'hello'],
+                                    'cafe' : ['cafe', 'campus', 'tea', 'coffee', 'eat'],
+				    'hello': ['hi', 'hey', 'hello'],
                                     'help' : ['help me', 'can help', 'help'],
                                     'name' : ['your name', 'name'],                             
-                                    'what' : ['what is', 'what is your', 'what'],
                                     'wash' : ['washroom', 'toilet'],
-                                    'library' : ['library'],
+                                    'library' : ['library', 'book', 'borrow'],
                                     'labs' : ['labs'],
                                     'talk': ['talk to me?', 'really talk?', 'you talk', 'you really talk?', 'talk'],                      
-                                    'how' : ['can you', 'can', 'how can', 'how can you'],
-                                    'cute' : ['cute', 'so cute'],
-                                    'amazing' : ['amazing', 'wonderful'],                                    
+                                    'amazing' : ['amazing', 'wonderful'], 
+				    'psychology' : ['psychology'],
+				    'teaching' : ['teaching', 'music'],
+				    'engineering' : ['engineering'],
+				    'biology' : ['biology', 'english', 'chemistry'],
+				    'maths' : ['computing', 'mathematics'],
+				    'geo' : ['geology', 'geography'],
+				    'marine' : ['marine'],
+				    'art' : ['art'],
+    				    'roland' : ['reception', 'architecture'],
+				    'business' : ['business'],
+				    'staff' : ['staff'],
+				    'sports' : ['sports'],
+  				    'robots' : ['robotics', 'robots'],
+				    'visit' : ['visit', 'to do'],
+				    'supermarket' : ['shop', 'supermarket'],
+			     	    'cashpoint' : ['cash points', 'ATM', 'cash machines'],
+				    'day' : ['day', 'today'],
+				    'weather' : ['weather'],                                 
                                     'pause': ['pause speech'],
                                     'continue': ['continue speech']}
         
@@ -71,7 +74,6 @@ class talkative:
         
         # We have to keep publishing the cmd_vel message if we want the robot to keep moving.
         while not rospy.is_shutdown():
-            self.cmd_vel_pub.publish(self.msg)
             r.sleep()
    	
             
@@ -94,48 +96,96 @@ class talkative:
         if self.paused:
             return       
         
-        if command == 'hello':    
-            self.soundhandle.say("Greetings! from your very own Charlie. How may I assist you?.", self.voice)
-            rospy.sleep(3)
+        if command == 'hello':
+		self.soundhandle.say("Greetings! from your very own Charlie. How may I assist you?.", self.voice)
+		rospy.sleep(3)
             
         elif command == 'bye':
-        	self.soundhandle.say("Goodbye! See you soon.", self.voice)
+        	self.soundhandle.say("Ok! See you soon. Have a Nice Day!. ", self.voice)
         	rospy.sleep(2)
         
         if command == 'help':
-        	self.soundhandle.say("I am a receptionist robot. I can answer your questions about the faculty. or guide you through the faculty building.", self.voice)
-        	
-                    	
+        	self.soundhandle.say("I am a receptionist robot. I can answer your questions about the faculty. or guide you through the faculty building!.", self.voice)
+        	                    	
         if command == 'talk':
         	self.soundhandle.say("Oh! I am talking.. Have you seen my brother Wall E?.", self.voice)
         	        	
-		
+	if command == 'weather':
+		self.soundhandle.say("Why don't you look out of the window?!. It is probably raining now!", self.voice)
         
-        if command == 'cute':
-        	self.soundhandle.say("Oh my God! I am blushing. You are very kind", self.voice)
-            
+	if command == 'supermarket':
+		self.soundhandle.say("The nearest one is the TESCO, over the road, by the Reynold's building!. ", self.voice)
+	
+	if command == 'day':
+		self.soundhandle.say("Today is tuesday!. Oh my God! it's still 3 days to the weekend", self.voice)
+
+	if command == 'psychology':
+		self.soundhandle.say("The psychology department is in the link building! You can get there! from the second floor of this building.!", self.voice)
+
+	if command == 'teaching':
+		self.soundhandle.say("The department is in the rolle building! You can find it on the north-west of the campus.!", self.voice)
+
+	if command == 'engineering':
+		self.soundhandle.say("That's right here! In the Smeaton's building! There are also brunel labs behind the library.!", self.voice)
+
+	if command == 'biology':
+		self.soundhandle.say("The department is in the Davy building! You can find it! across from the library over the plaza.!", self.voice)
+	
+	if command == 'maths':
+		self.soundhandle.say("The department is in the babbage building! Leave via the western exit and take right across the road.!", self.voice)
+
+	if command == 'geo':
+		self.soundhandle.say("The department is in the Fitzroy building! You can find it north of the library beyond the trees!", self.voice)
+
+	if command == 'marine':
+		self.soundhandle.say("The marine department is in the reynolds building! And also the marine building where you can find the test tank, and the bridge simulators.!. It's across the road to the west of this building", self.voice)
+	
+	if command == 'art':
+		self.soundhandle.say("The art faculty is in the scott building! You can find it directly south of the Smeaton's building.!", self.voice)
+	
+	if command == 'roland':
+		self.soundhandle.say("That is in the roland levinsky building! You can find it directly south of the Smeaton's building.!", self.voice)
+	
+	if command == 'business':
+		self.soundhandle.say("I believe that's in the cookworthy building! I have no memory of this place!", self.voice)
+
+	if command == 'staff':
+		self.soundhandle.say("You can find various members of staff in the Portland Square building!", self.voice)
+
+	if command == 'sports':
+		self.soundhandle.say("The sports facility is in the Nancy Astor building! You can find it on the northern edge of campus past Portland Square.!", self.voice)
+
+	if command == 'robots':
+		self.soundhandle.say("You can learn about robots here in Smeaton's building or in Portland Square. !", self.voice)	
+
+	if command == 'cashpoint':
+		self.soundhandle.say("There are cashpoints on the eastern exit of this building.! Don't forget your credit card!", self.voice)
+
+	if command == 'visit':
+		self.soundhandle.say("You can visit gin distillery or aquarium or go on a tour of The Sound.!", self.voice)
+                    
         if command == 'name':
-        	self.soundhandle.say("They named me charlie. As if I wanted it. ", self.voice)
+        	self.soundhandle.say("My name is CHARLIE.! CHArismatic Robot with Linguistic Interface . ", self.voice)
        	
        	if command == 'amazing':
        		self.soundhandle.say("Not as much as you are. But thank you so much.", self.voice)           
         
         if command == 'cafe':
-        	self.soundhandle.say("There is one S U shop right opposite to the library. you can eat or have coffee there", self.voice)      
+        	self.soundhandle.say("There is one S U shop right opposite to the library. you can eat! or have a coffee there!", self.voice)      
        
         if command == 'wash':
         	self.soundhandle.say("The Gents toilet can be found on the second floor. and a ladies toilet on the third floor.", self.voice)
     
         if command == 'library':
-        	self.soundhandle.say("As you will exit out of the Smeaton's building. Take a left. The building in front of you is the library. ", self.voice)
+        	self.soundhandle.say("As you will exit, out of eastern end of this building!. Take a left!. The building! in front of you! is the library!. ", self.voice)
         
         if command == 'labs':
         	self.soundhandle.say("The labs are on third floor of the Smeaton's building. Infact, we might be standing in one of those. ", self.voice)
     
+
     def cleanup(self):
-        # When shutting down be sure to stop the robot!  Publish a Twist message consisting of all zeros.
-        twist = Twist()
-        self.cmd_vel_pub.publish(twist)
+        # When shutting down
+	rospy.loginfo("Shutting Down...")
 		
 		
 if __name__=="__main__":
